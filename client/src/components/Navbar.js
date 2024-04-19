9ea0c4141a68f61047c2e278408fcb9e50da95b4
rajeshwariT20 <rajeshwari.thepale@gmail.com>
2024-04-19 18:57:18 +0530

    daily updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,28 +6,32 @@ import { Link } from "react-router-dom";
 export default function Navbar(){
 
     return(
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand logo " href="#">OrganicFood</a>
-            <div className="collapse navbar-collapse justify-content-end" id="navbar">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">Home </Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link className="nav-link" to="/">Freshly Made</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="">Shop</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="">Blog</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="">Contact</Link>
-                    </li>
-                </ul>
-                <Search></Search>
-            </div>
-        </nav>
+        <div className="menu-wrapper sticky-top">
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <a className="navbar-brand logo " href="#">OrganicFood</a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link className="nav-link" to="/">Freshly Made</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="">Shop</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="">Blog</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="">Contact</Link>
+                        </li>
+                    </ul>
+                    <Search></Search>
+                </div>
+            </nav>
+        </div>
+        </div>
     )
 }
